73e6502543c8ed07bcbd8baaa2bb3ff2c6a5437f
devmicros <developers.microsafe@gmail.com>
2025-08-02 21:19:50 -0600

    Actualiza contenedores

--- helm-check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#git clone https://github.com/techiescamp/helm-tutorial.git
-#helm create aims-chart
-
-cd aims-chart
-helm lint .
-helm template .
-cd ..
-helm install --dry-run my-release aims-chart
